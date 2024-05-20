7bb3f48fb9b7514e16e19751d37e4f801631e6f3
nishulabiba <nishlabiba@gmail.com>
2024-05-21 01:07:31 +0600

    product service created

---
+++ src/app/modules/products/produuct.service.ts
@@ -0,0 +1,15 @@
+import { Product } from "./product.interface";
+import { ProductModel } from "./produuct.model";
+
+const createProductIntoDB = async (product: Product)=>{
+    const result= await ProductModel.create(product)
+    return result;
+ 
+ }
+ const getProductfromDB = async ()=>{
+     const result= await ProductModel.find()
+     return result;
+  
+  }
+ 
+ export const ProductRelatedServices={ createProductIntoDB, getProductfromDB }
